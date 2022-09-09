54b2588727ad1cf84aa26b498e34a2ae631b71cb
Vladimir Manzhos <manzhos@gmail.com>
2022-09-09 09:45:26 +0200

    modified:   _source/mm.ai 	modified:   src/App.js 	new file:   src/login.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+// https://api.instagram.com/oauth/authorize?app_id=822188005463007&redirect_uri=https%3A%2F%2Fmultimix.vercel.app%2Flogin&scope=user_profile,user_media&response_type=code
+
+
 import './App.css';
 
 function App() {
+  const appId       = 822188005463007,
+        redirectUri = "https%3A%2F%2Fmultimix.vercel.app",
+        authLink    = "https://api.instagram.com/oauth/authorize?app_id=" + appId + "&redirect_uri=" + redirectUri + "&scope=user_profile,user_media&response_type=code"
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi man.</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={authLink}
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/login.js
@@ -0,0 +1,15 @@
+const Login = () => {
+  <div>
+    <h2>Login</h2>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Press here to login
+    </a>
+  </div>
+}
+
+export default Login;
